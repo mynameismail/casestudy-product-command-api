416b14d2bc7a7f750c0f8936c6f90f9501c9c867
Ismail <ismailsmakhus@gmail.com>
2023-04-16 14:00:13 +0700

    post category route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Middleware\VerifyApiKey;
+use App\Http\Controllers\CategoryController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(VerifyApiKey::class)->group(function () {
+    Route::post('/categories', [CategoryController::class, 'add']);
 });
