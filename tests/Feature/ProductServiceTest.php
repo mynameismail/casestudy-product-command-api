e7917a404ea99bc83bd5e9930016cd7542e2ade0
Ismail <ismailsmakhus@gmail.com>
2023-04-17 12:16:02 +0700

    product service test

---
+++ tests/Feature/ProductServiceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Category;
+use App\Services\ProductService;
+
+class ProductServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test add product, it should persist add product and return product object correctly
+     */
+    public function test_add(): void
+    {
+        $category = Category::factory()->create();
+
+        $payload = [
+            'sku' => 'abc',
+            'name' => 'test product',
+            'price' => 1000,
+            'stock' => 1000,
+            'categoryId' => $category->id,
+        ];
+        
+        $product = (new ProductService())->add($payload);
+
+        $this->assertNotEmpty($product->id);
+        $this->assertEquals($product->sku, $payload['sku']);
+        $this->assertEquals($product->name, $payload['name']);
+        $this->assertEquals($product->price, $payload['price']);
+        $this->assertEquals($product->stock, $payload['stock']);
+        $this->assertEquals($product->category_id, $payload['categoryId']);
+        $this->assertNotEmpty($product->created_at);
+    }
+}
